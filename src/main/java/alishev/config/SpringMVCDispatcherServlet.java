3ba2a41efbf2fcfa72a70aa0b9cb4eba9a57e9c2
Evgeny Voinov <joekobarnd@gmail.com>
2022-11-25 17:28:36 +0300

    1st

---
+++ src/main/java/alishev/config/SpringMVCDispatcherServlet.java
@@ -0,0 +1,20 @@
+package alishev.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMVCDispatcherServlet extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>SOSI JOPU, GOVNO!</h2>
+</body>
+</html>
